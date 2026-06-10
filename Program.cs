cae355d93f417d2bb3a6127e196e0cf3ee674f7b
Micaela Tamires <micaela.tamir@gmail.com>
2026-06-10 15:39:21 -0300

    register repositories and services in dependency injection container
    - added AddScoped registrations for ServiceRepository, MetricRepository and IncidentRepository
    - added AddScoped registrations for ServiceService, MetricService and IncidentService
    - all layers now wired through interfaces, no concrete dependencies exposed

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
 using DevPulseApi.Data;
+using DevPulseApi.Repositories;
+using DevPulseApi.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
+builder.Services.AddScoped<IMetricRepository, MetricRepository>();
+builder.Services.AddScoped<IIncidentRepository, IncidentRepository>();
+
+builder.Services.AddScoped<IServiceService, ServiceService>();
+builder.Services.AddScoped<IMetricService, MetricService>();
+builder.Services.AddScoped<IIncidentService, IncidentService>();
 
 builder.Services.AddOpenApi();
 
